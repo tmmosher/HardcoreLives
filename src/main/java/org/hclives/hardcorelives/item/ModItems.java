e0149ae216abec34b61ba53dbd4e25490244344a
Trenton Mosher <starkbewegung@hotmail.com>
2025-08-01 23:53:20 -0700

    Artifact models load in! Turns out that there is now a items/ directory that needs to have model data for models to appear in game. Very little documentation reflects this.

--- src/main/java/org/hclives/hardcorelives/item/ModItems.java
+++ src/main/java/org/hclives/hardcorelives/item/ModItems.java
@@ -6,24 +6,26 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.hclives.hardcorelives.Hardcorelives;
 
 public class ModItems {
     // increase lives by two
-    public static final Item PHOENIX_FLOWER = registerItem("phoenix_flower", new Item(new Item.Settings()));
+    public static final Item PHOENIX_FLOWER = registerItem("phoenix_flower");
     // throw and teleport to the location where it lands, after a brief delay
-    public static final Item ROD_OF_NARSUS  = registerItem("rod_of_narsus", new Item(new Item.Settings()));
+    public static final Item ROD_OF_NARSUS  = registerItem("rod_of_narsus");
     // every hour, summon a stack of oak wood logs
-    public static final Item WOODLAND_HEART = registerItem("woodland_heart", new Item(new Item.Settings()));
+    public static final Item WOODLAND_HEART = registerItem("woodland_heart");
     // pulls a player toward you from great distance
-    public static final Item CHAIN_OF_FATE = registerItem("chain_of_fate", new Item(new Item.Settings()));
+    public static final Item CHAIN_OF_FATE = registerItem("chain_of_fate");
     // chance of parrying an opponent's attack
-    public static final Item FARRON_DAGGER = registerItem("farron_dagger", new Item(new Item.Settings()));
+    public static final Item FARRON_DAGGER = registerItem("farron_dagger");
     // sunder your lungs with unholy fury, slowing down every player around you and speeding yourself up
-    public static final Item CELERITAL_SCROLL = registerItem("celerital_scroll", new Item(new Item.Settings()));
+    public static final Item CELERITAL_SCROLL = registerItem("celerital_scroll");
     // freeze time, reducing tick speed to 0 and giving all other players slowness X for two seconds
-    public static final Item ERGO_RUNE = registerItem("ergo_rune", new Item(new Item.Settings()));
+    public static final Item ERGO_RUNE = registerItem("ergo_rune");
     // and many more...
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
@@ -36,8 +38,11 @@ public class ModItems {
         entries.add(ERGO_RUNE);
     }
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM,  Identifier.of(Hardcorelives.MOD_ID, name), item);
+    private static Item registerItem(String name) {
+        // may need to refactor the 'settings' handling as per https://fabricmc.net/2024/10/14/1212.html
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Hardcorelives.MOD_ID, name));
+        Item item = new Item(new Item.Settings().registryKey(key));
+        return Registry.register(Registries.ITEM, key, item);
     }
 
     public static void registerModItems() {
