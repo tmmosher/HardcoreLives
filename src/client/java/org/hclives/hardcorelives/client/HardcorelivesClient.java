1de6c7a8d21472d8305377512ade59751170e7f6
Trenton Mosher <starkbewegung@hotmail.com>
2025-08-01 19:16:53 -0700

    init

---
+++ src/client/java/org/hclives/hardcorelives/client/HardcorelivesClient.java
@@ -0,0 +1,10 @@
+package org.hclives.hardcorelives.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HardcorelivesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/hclives/hardcorelives/Hardcorelives.java
@@ -0,0 +1,14 @@
+package org.hclives.hardcorelives;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hardcorelives implements ModInitializer {
+    public static String MOD_ID = "hardcorelives";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+
+    }
+}
